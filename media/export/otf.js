aa3ec552782951e5769d5005eaa3cac010102c61
inventionalt <masiainzot@educacion.navarra.es>
2026-07-09 20:22:35 +0200

    Update otf.js

--- media/export/otf.js
+++ media/export/otf.js
@@ -2,9 +2,7 @@
 
 const align4 = (x)=>(x+3) & ~3;
 
-function getChecksum(buffer, offset, length) {
-  const view = new DataView(buffer);
-
+function getChecksum(view, offset, length) {
   let sum = 0;
   let padded = align4(length);
 
@@ -14,7 +12,6 @@ function getChecksum(buffer, offset, length) {
     if (i+1 < length) word |= view.getUint8(offset+i+1) << 16;
     if (i+2 < length) word |= view.getUint8(offset+i+2) << 8;
     if (i+3 < length) word |= view.getUint8(offset+i+3);
-
     sum = (sum + (word >>> 0)) >>> 0;
   }
 
@@ -24,24 +21,55 @@ function getChecksum(buffer, offset, length) {
 // Tables
 const tableGen = {
   cmap: (view, offset, glyphs, substitutions)=>{
+    offset = align4(offset);
+    const start = offset;
+    let has4subtable = glyphs.filter(gl=>gl.name.length===1&&gl.name.codePointAt(0)=>0xFFFF).length>0;
+    let has12subtable = glyphs.filter(gl=>gl.name.length===1&&gl.name.codePointAt(0)=>0xFFFF).length>0;
+    let has14subtable = glyphs.filter(gl=>gl.name.length>1).length>0;
+
     view.setUint16(offset, 0, false); // version
     offset += 2;
-    view.setUint16(offset, 1, false); // numTables (think one unicode is enough, we def need more)
+    view.setUint16(offset, has4subtable+has12subtable+has14subtable, false); // numTables
     offset += 2;
 
-    view.setUint16(offset, 0, false); // platformID
-    offset += 2;
-    view.setUint16(offset, 5, false); // encodingID
-    offset += 2;
-    view.setUint32(offset, 12, false); // subtableOffset
-    offset += 4;
+    let subtableIdxStart = offset;
+    if (has4subtable) {
+      view.setUint16(offset, 0, false); // platformID
+      offset += 2;
+      view.setUint16(offset, 3, false); // encodingID
+      offset += 2;
+      view.setUint32(offset, 12, false); // subtableOffset
+      offset += 4;
+    }
+    if (has12subtable) {
+      view.setUint16(offset, 0, false); // platformID
+      offset += 2;
+      view.setUint16(offset, 4, false); // encodingID
+      offset += 2;
+      view.setUint32(offset, 12, false); // subtableOffset
+      offset += 4;
+    }
+    if (has14subtable) {
+      view.setUint16(offset, 0, false); // platformID
+      offset += 2;
+      view.setUint16(offset, 5, false); // encodingID
+      offset += 2;
+      view.setUint32(offset, 12, false); // subtableOffset
+      offset += 4;
+    }
 
-    view.setUint16(offset, 14, false); // format
+    /*view.setUint16(offset, 14, false); // format
     offset += 2;
     view.setUint32(offset, 0, false); // TODO: length
     offset += 4;
     view.setUint32(offset, 0, false); // TODO: numVarSelectorRecords
-    offset += 4;
+    offset += 4;*/
+
+    return {
+      start,
+      offset,
+      length: offset-start
+    };
   }
 };
 
@@ -93,5 +121,12 @@ export function generateOTF(glyphs, substitutions) {
     offset += 4;
   }
 
-
+  for (let i=0; i<tables.length; i++) {
+    let ret = tableGen[tables[i]](view, offset, glyphs, substitutions);
+    offset = directoryStart+(16*i);
+    view.setUint32(offset+12, ret.length, false);
+    view.setUint32(offset+8, ret.start, false);
+    view.setUint32(offset+4, getChecksum(view, ret.start, ret.length), false);
+    offset = ret.offset;
+  }
 }
