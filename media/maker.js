f4cb7a7640f2ba9b49ae2e0f25fa7bee290717b3
inventionalt <masiainzot@educacion.navarra.es>
2026-07-09 13:47:30 +0200

    Update maker.js

--- media/maker.js
+++ media/maker.js
@@ -8,15 +8,23 @@ let glyphs = [];
 let substitutions = [];
 function showGlyphLists() {
   let disp = (gl)=>`<div>
-  <img src="${gl.glyf}">
+  <!--img src="${gl.glyf}"-->
+  <span>${fl.glyf}</span>
   <span>${gl.name}</span>
 </div>`;
   document.getElementById('glyph-list').innerHTML = glyphs.map(disp).join('');
   document.getElementById('sub-list').innerHTML = substitutions.map(disp).join('');
 }
 window.createGlyph = ()=>{
+  let char = prompt('Character');
+  if ([...Intl.Segmenter(undefined, {
+    granularity: 'grapheme'
+  }).segment(char)].length!==1) {
+    alert('Only one grapheme allowed');
+    return;
+  }
   glyphs.push({
-    name: prompt('Character'),
+    name: char,
     glyf: ''
   });
   showGlyphLists();
@@ -35,5 +43,5 @@ window.exportShow = ()=>{
   document.getElementById('export-modal').showModal();
 };
 window.exportFont = ()=>{
-  generateOTF();
+  generateOTF(glyphs, substitutions);
 };
